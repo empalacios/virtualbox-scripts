028a7d3279ad09aef9fc92fcd0ccc6d7b48cb9b7
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-02-13 14:29:32 -0600

    Create vm with specified parameters from cli and validate them.

--- crear_maquina.sh
+++ crear_maquina.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
-vbox_dir=/home/user/VirtualBox\ VMs
-vm_name=alpine
-# one of `vboxmanage list ostypes`
-ostype=Linux26_64
-memory=512
-disk_name=$vm_name.vdi
-disk_size=102400
-so_installation_image=/home/user/Downloads/alpine-standard-3.8.0-x86_64.iso
+MINPARAMS=6
 
-vboxmanage showvminfo alpine &>/dev/null
+if [ $# -lt "$MINPARAMS" ]
+then
+  echo "Usage $0 vm_name vms_directory ostype disk_size ram_size install_image"
+  echo "  vm_name:       Virtual machine's name"
+  echo "  vms_directory: Base directory where the virtual machine's directory will be created"
+  echo "  ostype:        Virtual machine's os type, one of \`vboxmanage list ostypes\`"
+  echo "  disk_size:     Capacity of the virtual machine's drive, in Megabytes"
+  echo "  ram_size:      Capacity of the virtual machine's RAM, in Megabytes"
+  echo "  install_image: Path to the installation image (ISO)"
+  exit 1
+fi
+
+
+vm_name=${1}
+vms_directory=${2}
+ostype=${3}
+disk_size=${4}
+ram_size=${5}
+installation_image=${6}
+disk_name=$vm_name.vdi
+
+
+if [ ! -d "$vms_directory" ]
+then
+  echo "Error: base virtual machines directory $vms_directory doesn't exist."
+  exit 1
+fi
+
+if [ "$disk_size" -le "0" ]
+then
+  echo "Error: disk capacity must be greater than zero (0) Megabytes"
+  exit 1
+fi
+
+if [ "$ram_size" -le "0" -o "$ram_size" -gt "4096" ]
+then
+  echo "Error: RAM capacity must be a value between one (1) and four thousand ninety six (4096) Megabytes (1 Mb - 4 Gigabytes)"
+  exit 1
+fi
+
+if [ ! -f "$installation_image" ]
+then
+  echo "Error: installation image $installation_image doesn't exist."
+  exit 1
+fi
+
+vboxmanage showvminfo $vm_name &>/dev/null
 if [ $? -eq 0 ]
 then
   vboxmanage unregistervm $vm_name --delete
   echo "Deleted $vm_name"
 fi
+
+
 vboxmanage createvm --name $vm_name --register --ostype $ostype
 
-vboxmanage modifyvm $vm_name --memory $memory \
+vboxmanage modifyvm $vm_name --memory $ram_size \
   --vram 128 \
   --audiocontroller hda \
   --audiocodec stac9221 \
@@ -29,9 +70,9 @@ vboxmanage modifyvm $vm_name --vrde on
 
 #nat networking
 vboxmanage modifyvm $vm_name --nic1 nat --cableconnected1 on
-echo "Created vm $vm_name: $memory MB RAM"
+echo "Created vm $vm_name: $ram_size MB RAM"
 
-vboxmanage createhd --filename "$vbox_dir/$vm_name/$disk_name" \
+vboxmanage createhd --filename "$vms_directory/$vm_name/$disk_name" \
   --size $disk_size
 vboxmanage storagectl $vm_name \
   --name 'SATA Controller' \
@@ -43,8 +84,8 @@ vboxmanage storageattach $vm_name \
   --port 0 \
   --device 0 \
   --type hdd \
-  --medium "$vbox_dir/$vm_name/$disk_name"
-echo "Created drive for $vm_name: $disk_size MB, location $vbox_dir/$vm_name/$disk_name"
+  --medium "$vms_directory/$vm_name/$disk_name"
+echo "Created drive for $vm_name: $disk_size MB, location $vms_directory/$vm_name/$disk_name"
 vboxmanage storagectl $vm_name \
   --name 'IDE Controller' \
   --add ide
@@ -53,15 +94,15 @@ vboxmanage storageattach $vm_name \
   --port 0 \
   --device 0 \
   --type dvddrive \
-  --medium "$so_installation_image"
-echo "Created cdrom drive for $vm_name: $disk_size MB, location $so_installation_image"
+  --medium "$installation_image"
+echo "Created cdrom drive for $vm_name: $disk_size MB, mounted image $installation_image"
 
 echo "Next steps"
 echo "Start vm without GUI: vboxmanage startvm $vm_name --type=headless"
 echo "View vm info:         vboxmanage showvminfo $vm_name"
 echo "After installation"
-echo "Remove Installation image from CDROM: vboxmanage storageattach alpine --storagectl 'IDE Controller' --port 0 --device 0 --medium none"
-echo "Add image to CDROM Drive:             vboxmanage storageattach alpine --storagectl 'IDE Controller' --port 0 --device 0 --type dvddrive --medium <imagepath>"
-echo "Remove CDROM Drive:                   vboxmanage storagectl alpine --name 'IDE Controller' --remove"
+echo "Remove Installation image from CDROM: vboxmanage storageattach $vm_name --storagectl 'IDE Controller' --port 0 --device 0 --medium none"
+echo "Add image to CDROM Drive:             vboxmanage storageattach $vm_name --storagectl 'IDE Controller' --port 0 --device 0 --type dvddrive --medium <imagepath>"
+echo "Remove CDROM Drive:                   vboxmanage storagectl $vm_name --name 'IDE Controller' --remove"
 echo "Add Bidirectional Clipboard:          vboxmanage controlvm $vm_name clipboard bidirectional"
 echo "Disconnect from network:              vboxmanage modifyvm $vm_name --nic1 null --cableconnected1 off"
